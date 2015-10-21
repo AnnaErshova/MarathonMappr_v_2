cd2f9625bedf65183582dc98ee4a2eb5d96c2182
AnnaErshova <anna.ershova@gmail.com>
2015-10-21 16:54:53 -0400

    another attempt at seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 require 'csv'
-csv_text = File.read('./seeds_data/marathon_list_oct_21_15.csv')
+csv_text = File.read('db/seeds_data/marathon_list_oct_21_15.csv')
 csv = CSV.parse(csv_text, :headers => true)
 csv.each do |row|
   Marathon.create!(row.to_hash)
